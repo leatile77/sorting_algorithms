3e7d99849f07dd01f18433e2211a5f20337a1ca9
leatile77 <moilaleatile@gmail.com>
2024-01-24 13:09:52 +0200

    shell sort

--- sort.h
+++ sort.h
@@ -18,11 +18,13 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
+void swap(int *x, int *y);
 void node_swap(listint_t **head, listint_t *swap1, listint_t *swap2);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
